bc03b2d469b92f2fbf185a01f1f45ccaa3a6a85e
Anne-DEHAEZE <anne.dehaeze@yahoo.fr>
2024-11-04 15:50:30 +0100

    halloween

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = ()) =>{
         const navMenu = document.getElementById('nav-menu')
         // when we click on each nav__link, we remove the show-menu class
             navMenu.classList.remove('show-menu')
